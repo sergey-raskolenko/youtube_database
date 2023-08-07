620a92cf9873b5c248fd5903fb876cdf972263ad
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-08-07 23:59:21 +0300

    Created create_database func in utils.py

--- utils.py
+++ utils.py
@@ -27,3 +27,43 @@ def get_youtube_data(api_key: str, channel_ids: list[str]) -> list[dict[str, Any
         })
 
     return data
+
+def create_database(database_name: str, params: dict):
+    """Создание базы данных и таблиц для сохранения данных о каналах и видео."""
+
+    conn = psycopg2.connect(dbname='postgres', **params)
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    cur.execute(f"DROP DATABASE {database_name}")
+    cur.execute(f"CREATE DATABASE {database_name}")
+
+    conn.close()
+
+    conn = psycopg2.connect(dbname=database_name, **params)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE channels (
+                channel_id SERIAL PRIMARY KEY,
+                title VARCHAR(255) NOT NULL,
+                views INTEGER,
+                subscribers INTEGER,
+                videos INTEGER,
+                channel_url TEXT
+            )
+        """)
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            CREATE TABLE videos (
+                video_id SERIAL PRIMARY KEY,
+                channel_id INT REFERENCES channels(channel_id),
+                title VARCHAR NOT NULL,
+                publish_date DATE,
+                video_url TEXT
+            )
+        """)
+
+    conn.commit()
+    conn.close()
